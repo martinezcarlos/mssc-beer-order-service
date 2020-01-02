81f517830183dcf941610d1747c0504e2ea2b471
martinezcarlos <ingeniero.carlos.martinez@gmail.com>
2020-01-02 16:47:33 +0100

    - Moves common JMS events to a common folder under src/main/java. This is because the default JMS converter uses package naming convention to serialize and deserialize messages into and from the queue.

--- src/main/java/guru/sfg/beer/order/service/web/controllers/BeerOrderController.java
+++ src/main/java/guru/sfg/beer/order/service/web/controllers/BeerOrderController.java
@@ -20,55 +20,65 @@ package guru.sfg.beer.order.service.web.controllers;
 import guru.sfg.beer.order.service.services.BeerOrderService;
 import guru.sfg.beer.order.service.web.model.BeerOrderDto;
 import guru.sfg.beer.order.service.web.model.BeerOrderPagedList;
+import java.util.UUID;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/customers/{customerId}/")
 @RestController
 public class BeerOrderController {
 
-    private static final Integer DEFAULT_PAGE_NUMBER = 0;
-    private static final Integer DEFAULT_PAGE_SIZE = 25;
+  private static final Integer DEFAULT_PAGE_NUMBER = 0;
+  private static final Integer DEFAULT_PAGE_SIZE = 25;
 
-    private final BeerOrderService beerOrderService;
+  private final BeerOrderService beerOrderService;
 
-    public BeerOrderController(BeerOrderService beerOrderService) {
-        this.beerOrderService = beerOrderService;
+  public BeerOrderController(final BeerOrderService beerOrderService) {
+    this.beerOrderService = beerOrderService;
+  }
+
+  @GetMapping("orders")
+  public BeerOrderPagedList listOrders(@PathVariable("customerId") final UUID customerId,
+      @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+      @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+
+    if (pageNumber == null || pageNumber < 0) {
+      pageNumber = DEFAULT_PAGE_NUMBER;
     }
 
-    @GetMapping("orders")
-    public BeerOrderPagedList listOrders(@PathVariable("customerId") UUID customerId,
-                                         @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
-                                         @RequestParam(value = "pageSize", required = false) Integer pageSize){
-
-        if (pageNumber == null || pageNumber < 0){
-            pageNumber = DEFAULT_PAGE_NUMBER;
-        }
-
-        if (pageSize == null || pageSize < 1) {
-            pageSize = DEFAULT_PAGE_SIZE;
-        }
-
-        return beerOrderService.listOrders(customerId, PageRequest.of(pageNumber, pageSize));
+    if (pageSize == null || pageSize < 1) {
+      pageSize = DEFAULT_PAGE_SIZE;
     }
 
-    @PostMapping("orders")
-    @ResponseStatus(HttpStatus.CREATED)
-    public BeerOrderDto placeOrder(@PathVariable("customerId") UUID customerId, @RequestBody BeerOrderDto beerOrderDto){
-        return beerOrderService.placeOrder(customerId, beerOrderDto);
-    }
+    return beerOrderService.listOrders(customerId, PageRequest.of(pageNumber, pageSize));
+  }
 
-    @GetMapping("orders/{orderId}")
-    public BeerOrderDto getOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
-        return beerOrderService.getOrderById(customerId, orderId);
-    }
+  @PostMapping("orders")
+  @ResponseStatus(HttpStatus.CREATED)
+  public BeerOrderDto placeOrder(@PathVariable("customerId") final UUID customerId,
+      @RequestBody final BeerOrderDto beerOrderDto) {
+    return beerOrderService.placeOrder(customerId, beerOrderDto);
+  }
 
-    @PutMapping("/orders/{orderId}/pickup")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void pickupOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
-        beerOrderService.pickupOrder(customerId, orderId);
-    }
+  @GetMapping("orders/{orderId}")
+  public BeerOrderDto getOrder(@PathVariable("customerId") final UUID customerId,
+      @PathVariable("orderId") final UUID orderId) {
+    return beerOrderService.getOrderById(customerId, orderId);
+  }
+
+  @PutMapping("/orders/{orderId}/pickup")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void pickupOrder(@PathVariable("customerId") final UUID customerId,
+      @PathVariable("orderId") final UUID orderId) {
+    beerOrderService.pickupOrder(customerId, orderId);
+  }
 }
