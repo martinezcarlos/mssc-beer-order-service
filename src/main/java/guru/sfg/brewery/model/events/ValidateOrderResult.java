1df1f480632c39a8f6da4280cc33e3620ce49416
Carlos Martínez <carlosmm@ext.inditex.com>
2021-01-05 16:23:31 +0100

    Validate order listener.

---
+++ src/main/java/guru/sfg/brewery/model/events/ValidateOrderResult.java
@@ -0,0 +1,17 @@
+package guru.sfg.brewery.model.events;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidateOrderResult {
+  private UUID orderId;
+  private Boolean isValid;
+}
